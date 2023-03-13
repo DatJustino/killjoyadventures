7f930de95901489579b124a07ac440e47905501d
DatJustino <100353161+DatJustino@users.noreply.github.com>
2023-03-13 13:04:11 +0100

    init commit

---
+++ src/main/java/com/killjoy/killjoyadventures/KilljoyadventuresApplication.java
@@ -0,0 +1,13 @@
+package com.killjoy.killjoyadventures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KilljoyadventuresApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KilljoyadventuresApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/killjoy/killjoyadventures/KilljoyadventuresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.killjoy.killjoyadventures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KilljoyadventuresApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
